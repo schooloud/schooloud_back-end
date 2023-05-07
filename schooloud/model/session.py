c6e9be72208b1a4bbd8b0a73efb46abd6ef1ba97
yerim10044001 <rim0399@naver.com>
2023-05-07 18:13:28 +0900

    Fix: edit session model

--- schooloud/model/session.py
+++ schooloud/model/session.py
@@ -3,10 +3,8 @@ from schooloud.libs.database import db
 
 class Session(db.Model):
     __tablename__ = 'session'
-    sessionKey = db.Column(db.String(32), primary_key=True)
-    expiredAt = db.Column(db.DateTime(),nullable=False)
-
-    # userEmail = db.Column(db.String(50), db.ForeignKey("user.email"))
-    # user = db.relationship("Session", back_populates="user")  # many-to-one 관계의 참조 변수
+    session_key = db.Column(db.String(32), primary_key=True)
+    expired_at = db.Column(db.DateTime(), nullable=False)
+    user_email = db.Column(db.String(50), db.ForeignKey("user.email"))
 
     __table_args__ = {'extend_existing': True}
