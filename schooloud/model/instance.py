67b32d27268a0f0a21224ab4ea94793715e3e9a5
yerim10044001 <rim0399@naver.com>
2023-05-08 13:39:49 +0900

    Feat: update instance model attribute

--- schooloud/model/instance.py
+++ schooloud/model/instance.py
@@ -3,11 +3,17 @@ from schooloud.libs.database import db
 
 class Instance(db.Model):
     __tablename__ = 'instance'
-    instanceId = db.Column(db.String(32), primary_key=True)
+    instance_id = db.Column(db.String(32), primary_key=True)
     domain = db.Column(db.Text(100))
     port = db.Column(db.Integer)
-
-    # projectId = db.Column(db.String(32), db.ForeignKey("project.projectId"))
-    # project = db.relationship("Project", back_populates="instances")  # many-to-one 관계의 참조 변수
+    project_id = db.Column(db.String(32), db.ForeignKey("project.project_id"))
 
     __table_args__ = {'extend_existing': True}
+
+    def __init__(self, instance_id, port, project_id):
+        self.instance_id = instance_id
+        self.port = port
+        self.project_id = project_id
+
+    def as_dict(self):
+        return {x.name: getattr(self, x.name) for x in self.__table__.columns}
