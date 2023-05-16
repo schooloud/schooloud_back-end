8bd4527a7d27b6a2b9cfdb2f22633bda440745f1
yerim10044001 <rim0399@naver.com>
2023-05-16 14:57:39 +0900

    Add: session_authenticate to domain, flavor, instance API

--- schooloud/api/domain.py
+++ schooloud/api/domain.py
@@ -1,11 +1,13 @@
 from schooloud.blueprints import domain
 from schooloud.controller.DomainController import DomainController
+from schooloud.libs.decorator import session_authenticate
 from flask import request
 
 domainController = DomainController()
 
 
 @domain.route('/assign', methods=['POST'])
+@session_authenticate
 def assign_domain():
     params = request.get_json()
     response = domainController.assign_domain(params, request.cookies.get('email'))
@@ -13,6 +15,7 @@ def assign_domain():
 
 
 @domain.route('/list')
+@session_authenticate
 def get_domain_list():
     response = domainController.get_domain_list(request.cookies.get('email'))
     return response
--- schooloud/api/flavor.py
+++ schooloud/api/flavor.py
@@ -1,11 +1,13 @@
 from schooloud.blueprints import flavor
 from schooloud.controller.FlavorController import FlavorController
+from schooloud.libs.decorator import session_authenticate
 from flask import request
 
 flavorController = FlavorController()
 
 
 @flavor.route('/list')
+@session_authenticate
 def get_flavor_list():
     response = flavorController.get_flavor_list(request.cookies.get('email'))
     return response
