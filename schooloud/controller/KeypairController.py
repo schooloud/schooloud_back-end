01e63f4f7d49c140166bb1adf19ff47232ecc42f
niceyoo0221 <niceyoo0221@knu.ac.kr>
2023-05-19 20:32:33 +0900

    Fix: keypair error correction

--- schooloud/controller/KeypairController.py
+++ schooloud/controller/KeypairController.py
@@ -1,7 +1,10 @@
 from schooloud.controller.OpenStackController import OpenStackController
 
 from flask import jsonify
+
 openstack_controller = OpenStackController()
+
+
 class KeypairController:
     def __init__(self):
         pass
@@ -11,7 +14,7 @@ class KeypairController:
         conn = openstack_controller.create_connection(email)
         private_key = conn.create_keypair(body_params['keypair_name'])
 
-        return jsonify({"private_key":private_key.private_key})
+        return jsonify({"private_key": private_key.private_key})
 
     def keypair_list(self, email):
         conn = openstack_controller.create_connection(email)
@@ -19,9 +22,10 @@ class KeypairController:
         key_list = []
 
         for key in keys:
-            key_list.append(key.name)
+            key_list.append({
+                "keypair_name": key.name,
+                "finger_print": key.fingerprint,
+                "public_key": key.public_key
+            })
 
-        return jsonify({"key_list":key_list})
-
-
-
+        return jsonify({"key_list": key_list})
