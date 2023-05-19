10ababed1497e713871e0b388e26a32e174612b9
yerim10044001 <rim0399@naver.com>
2023-05-19 16:44:55 +0900

    Fix: get user information from session database

--- schooloud/libs/decorator.py
+++ schooloud/libs/decorator.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import request
 from schooloud.libs.database import db
+from schooloud.model.user import User
 from datetime import datetime
 from schooloud.model.session import Session
 from sqlalchemy.exc import NoResultFound
@@ -14,6 +15,12 @@ def session_authenticate(f):
             session = Session.query.filter(Session.session_key == session_key).one()
             if datetime.now() > session.expired_at:
                 return {"message": "session expired"}, 401
+
+            # get user information from session database
+            user = User.query.filter(User.email == session.user_email).one()
+            kwargs['email'] = session.user_email
+            kwargs['role'] = user.role
+            kwargs['name'] = user.name
         except NoResultFound:
             return {"message": "invalid session"}, 401
         return f(*args, **kwargs)
