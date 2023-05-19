0301d411806323b3c9f1979ca520409cc46a4dba
niceyoo0221 <niceyoo0221@knu.ac.kr>
2023-05-19 21:54:57 +0900

    create delete_keypair method

--- schooloud/api/keypair.py
+++ schooloud/api/keypair.py
@@ -16,7 +16,18 @@ def create_keypair(**kwargs):
     except:
         return abort(404)
 
+
 @keypair.route('/list')
 @session_authenticate
 def keypair_list(**kwargs):
     return keypairController.keypair_list(kwargs['email'])
+
+
+@keypair.route('/delete', methods=['POST'])
+@session_authenticate
+def delete_keypair(**kwargs):
+    try:
+        keypairController.delete_keypair(request.get_json(), kwargs['email'])
+        return Response("", status=200, mimetype='application/json')
+    except:
+        return abort(404)
